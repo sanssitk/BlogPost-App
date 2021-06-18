3da057bdf58a60f1b0fea5aa2e3b352590fefb7d
sanjay <sanssitk@gmail.com>
2021-06-17 19:04:20 -0500

    individual post onclick

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -14,15 +14,18 @@ const getPosts = () => {
     })
     .then((data) => {
       buildPosts(data);
-    }).catch (err => console.log(err))
+    })
+    .catch((err) => console.log(err));
 };
 
 const buildPosts = (blogPosts) => {
   let blogPostsContent = "";
   for (blogPost of blogPosts) {
     const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
-    const postImage = `${API_BASE_URL}${blogPost.post_image}`
+    const postImage = `${API_BASE_URL}${blogPost.post_image}`;
+    const postLink = `post.html?id=${blogPost.id}`;
     blogPostsContent += `
+    <a href="${postLink}" class="post-link">
         <div class="post">
           <div class="post-image" style="background-image: url(${postImage})"></div>
           <div class="post-content">
@@ -31,8 +34,10 @@ const buildPosts = (blogPosts) => {
             <div class="post-text">${blogPost.content}</div>
           </div>
         </div>
-        `
-    
+    </a>
+        
+        `;
+
     document.querySelector(".blog-posts").innerHTML = blogPostsContent;
   }
 };
--- exercises/js/post.js
+++ exercises/js/post.js
@@ -8,14 +8,39 @@ const API_URL = "http://localhost:3000/api/posts/";
 const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
-    getPost();
-}
+  getPost();
+};
+
+const getPostIdParam = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get("id");
+};
 
 const getPost = () => {
-    // CODE GOES HERE
-}
+  const postId = getPostIdParam();
+  // CODE GOES HERE
+  const url = `${API_URL}${postId}`;
+  fetch(url, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      buildPost(data);
+    })
+    .catch((err) => console.log(err));
+};
 
 const buildPost = (data) => {
-    // HINT: Convert the date number to a Date string 
-}
-
+  // HINT: Convert the date number to a Date string
+  const postDate = new Date(parseInt(data.added_date)).toDateString();
+  const postImage = `${API_BASE_URL}${data.post_image}`
+  document.querySelector("header").style.backgroundImage = `url(${postImage})`
+  document.getElementById("individual-post-title").innerText = data.title;
+  document.getElementById(
+    "individual-post-date"
+  ).innerText = `Published on: ${postDate}`;
+  document.getElementById("individual-post-content").innerText = data.content;
+};
